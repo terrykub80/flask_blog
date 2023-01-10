26b79caeed9a8e262807e624795ff5bcea526aef
Terry Kubalewski <terrykub@yahoo.com>
2023-01-10 14:04:17 -0600

    imported WTF and  added signupform

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.forms import SignUpForm
 
 @app.route('/')
 def index():
@@ -10,5 +11,12 @@ def index():
 
 @app.route('/posts')
 def posts():
-    return 'These are the posts'
+    return render_template('posts.html')
+
+
+@app.route('/signup')
+def signup():
+    # Create an instance of the SignUpForm
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
 
