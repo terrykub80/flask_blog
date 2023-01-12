2d0bd4e1a52758d6a8087438216900b3520da346
Terry Kubalewski <terrykub@yahoo.com>
2023-01-12 12:32:36 -0600

    added login

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 # Import SQLAlchemy and Migrate from their modules
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+# Import LoginManager from it's module --> In charge of login functionality
+from flask_login import LoginManager
 
 # Import Config Class from confix module - will have all of the app's configurations
 from config import Config
@@ -15,10 +17,12 @@ app = Flask(__name__)
 # Create the app using the config class
 app.config.from_object(Config)
 
-#Create and instance of the SQLAlchemy to represent our database
+# Create an instance of the SQLAlchemy to represent our database
 db = SQLAlchemy(app)
-#Create and instance of the Migrate to represent migration engine
+# Create an instance of the Migrate to represent migration engine
 migrate = Migrate(app, db)
+# Create an instance of LoginManager and pass in app
+login = LoginManager(app)
 
 # Import all of the routes and models from the routes and models files into the current folder
 from . import routes, models
